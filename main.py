55bdc6ceaf7619153bb23e5e410fb03c22eb83fb
BasavaGoni <basavarajgoni@gmail.com>
2025-02-09 19:09:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,40 +24,14 @@ tweet_chain = msg | gemini_model
 
 import streamlit as st
 
-st.markdown(
-    """
-    <style>
-    .stApp {
-        background-image: url("https://th.bing.com/th/id/R.4e8048922a137863c3918ca6a2b83bae?rik=N82vq8qNxd%2bm8g&riu=http%3a%2f%2fwww.superiorwallpapers.com%2fdownload%2ftwo-hearts-in-a-vector-and-design-wallpaper-5120x3200.jpg&ehk=VAUWTBl0KEXvGzpLWJa0vdRcEt3zFhBJMpHQrwARJTQ%3d&risl=&pid=ImgRaw&r=0");
-        background-attachment: fixed;
-        background-size: cover;
-    }
-    .stTextArea textarea, .stTextInput input {
-        text-align: left; 
-    }
-    label {
-        text-align: left;
-        float: left; 
-        margin-bottom: -15px; /* Adjust this value as needed */
-    }
-    div.stTextInput > label, div.stTextArea > label {
-        margin-top: -15px; /* Adjust this value as needed */
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-
 st.header("🐦Soul Note🐦")
+
 st.subheader("🤖 Generated a message for your love")
 
+From = st.text_input("From") To = st.text_input("To") Topic = st.text_input("Topic")
 
-From = st.text_input("From")
-To = st.text_input("To")
-Topic = st.text_input("Topic")
+if st.button("Generate"): tweets = tweet_chain.invoke({"From" : From, "To" : To, "Topic" : Topic}) st.write(tweets.content)
 
+import streamlit as st
 
-
-if st.button("Generate"):
-    tweets = tweet_chain.invoke({"From" : From, "To" : To, "Topic" : Topic})
-    st.write(tweets.content)
+st.markdown( f""" """, unsafe_allow_html=True )
